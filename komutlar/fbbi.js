db2f6f7aa4cb3aeb5427bbe25c55de63dacae1ba
Glitch (jewel-juicy-lyric) <none>
2022-04-11 10:55:07 +0000

    Rewound to commit 5b842a4508060a855bb6fc99ac6f7e72e4d003da + 0 edits

--- komutlar/fbbi.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-name:"fbi",
-code:`
-$title[1;**FBI Aç Kapıyı**]
-$description[1;**Açmazsan Uzaylılar Basacak Aç Kapıyı**]
-$image[1;https://media.tenor.com/images/a1912e38f72c5df9050d931853fafddb/tenor.gif]
-$color[1;120a8f]
-`
-  }
