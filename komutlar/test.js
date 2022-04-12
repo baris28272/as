7d04ebf835b0faa1fc8970a84785228ff9a5dbaa
Glitch (jewel-juicy-lyric) <none>
2022-04-12 15:04:40 +0000

    💰🐿 Checkpoint ./komutlar/test.js:38852640/540

--- komutlar/test.js
+++ komutlar/test.js
@@ -3,9 +3,20 @@ name:"test",
 code:`
 $color[1;RANDOM]1
 $author[1;**$userTag[$clientID] Botun İstatistikleri;$userAvatar[$clientID]]
+
+$addField[1;**Hosting Detayları**;
+<:emoji_75:963445326067425370> CPU Kullanımı : \`%$cpu\`
+<:emoji_75:963445326067425370> RAM Kullanımı : \`$ram\`
+<:emoji_75:963445326067425370> Uptime Süresi : \`$uptime\`
+]
+
 $addField[1;**Değeleri**;
 <:emoji_77:963445382933807216> Toplam Komutlar : \`$commandsCount\`
-<:emoji_77:963445382933807216> 
+<:emoji_77:963445382933807216> Toplam Sunucu : \`$serverCount\`
+<:emoji_77:963445382933807216> Toplam Kullanıcı : \`$allMembersCount\`
+<:emoji_77:963445382933807216> Toplam Kanal Sayım : \`$allChannelCount\`
+]
+
 $addField[1;**Geliştiricilerim**;
 **<:emoji_76:963445354626416650> $userTag[728155399655784488] - 728155399655784488**]
 
