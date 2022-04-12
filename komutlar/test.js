7d3330a60ddef5b39e48abebe972d670db924f44
Glitch (jewel-juicy-lyric) <none>
2022-04-12 14:52:40 +0000

    ⛴👸 Checkpoint ./komutlar/test.js:38852640/546

---
+++ komutlar/test.js
@@ -0,0 +1,13 @@
+module.exports = {
+name:"test",
+code:`
+$color[1;RANDOM]1
+$author[1;**$userTag[$clientID] Botun İstatistikleri;$userAvatar[$clientID]]
+$addField[1;**Değeleri**;
+<:emoji_77:963445382933807216> Toplam Komutlar : \`$commandsCount\`
+<:emoji_77:963445382933807216> 
+$addField[1;**Geliştiricilerim**;
+**<:emoji_76:963445354626416650> $userTag[728155399655784488] - 728155399655784488**]
+
+`
+  }
