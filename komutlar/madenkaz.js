f495866b75358f58db70debac61a744b1bf44cb9
Glitch (jewel-juicy-lyric) <none>
2022-04-13 06:44:01 +0000

    👲👡 Checkpoint ./komutlar/madenkaz.js:38852640/105

--- komutlar/madenkaz.js
+++ komutlar/madenkaz.js
@@ -1,7 +1,10 @@
 module.exports = {
   name: "maden-kaz",
   code: `
-**<@$authorID>, madenden \`$random[20;50]\` elmas ve \`$random[15;45]\` altın buldun.**
+$color[1;RANDOM]
+$author[1;$userTag;$userAvatar[$authorID]]
+$description[1;
+**<@$authorID>, madenden \`$random[20;50]\` elmas ve \`$random[15;45]\` altın buldun.**]
 $setGlobalUserVar[elmas;$sum[$getGlobalUserVar[elmas;$authorID];$random[20;50]];$authorID]
 $setGlobalUserVar[altın;$sum[$getGlobalUserVar[altın;$authorID];$random[15;45]];$authorID]
 $cooldown[2m;] 
