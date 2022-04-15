e5ec6557175e0274fd9365d80b938495102cfa78
Glitch (jewel-juicy-lyric) <none>
2022-04-15 14:43:20 +0000

    🏙🌉 Checkpoint ./komutlar/eval.js:38852640/220

--- komutlar/eval.js
+++ komutlar/eval.js
@@ -2,6 +2,5 @@ module.exports = {
   name: "eval",
   code: `
 $eval[$message]
-$onlyForIDs[$botOwnerID;728155399655784488;558683225007063040;777181429934194709;**$customEmoji[emoji_71] | Bu Komut Sadece \`$userTag[728155399655784488]\`'a Özeldir**]
 `,
 };
