f45aa8e608effa79ed7321810acaa31f786046ab
Glitch (jewel-juicy-lyric) <none>
2022-04-17 15:59:21 +0000

    🚆👞 Checkpoint ./komutlar/istatistik.js:38852640/415

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -4,26 +4,26 @@ code:`
 $color[1;RANDOM]
 $author[1;$username[$clientID] Botun İstatistikleri;$userAvatar[$clientID]]
 
-$addField[1;**・Sürümler**;
-**<:6149_discord:965276693604466758> Node.js Sürüm : \`v16.12.0\`
-<:6149_discord:965276693604466758> Aoi.js Sürüm : \`v$packageVersion\`**
+$addField[1;**<:aoijs:965279373005566052>・Sürümler**;
+> **<:6149_discord:965276693604466758> Node.js Sürüm : \`v16.12.0\`**
+> **<:6149_discord:965276693604466758> Aoi.js Sürüm : \`v$packageVersion\`**
 ]
 
 $addField[1;**・Hosting Detayları**;
-**<:6149_discord:965276693604466758> CPU Kullanımı : \`%$cpu\`
-<:6149_discord:965276693604466758> RAM Kullanımı : \`$ram\`
-<:6149_discord:965276693604466758> Depolama Kullanımı : \`25.93359375 MB\`**
+> **<:6149_discord:965276693604466758> CPU Kullanımı : \`%$cpu\`**
+> **<:6149_discord:965276693604466758> RAM Kullanımı : \`$ram\`**
+> **<:6149_discord:965276693604466758> Depolama Kullanımı : \`25.93359375 MB\`**
 ]
 
-$addField[1;**・Değeleri**;
-**<:emoji_77:963445382933807216> Toplam Komutlar : \`$commandsCount\`
-<:emoji_77:963445382933807216> Toplam Sunucu : \`$serverCount\`
-<:emoji_77:963445382933807216> Toplam Kullanıcı : \`$allMembersCount\`
-<:emoji_77:963445382933807216> Toplam Kanal Sayım : \`$allChannelsCount\`**
+$addField[1;**・Değerleri**;
+> **<:website:965278176383238215> Toplam Komutlar : \`$commandsCount\`**
+> **<:website:965278176383238215> Toplam Sunucu : \`$serverCount\`**
+> **<:website:965278176383238215> Toplam Kullanıcı : \`$allMembersCount\`**
+> **<:website:965278176383238215> Toplam Kanal Sayım : \`$allChannelsCount\`**
 ]
 
-$addField[1;**・Geliştiricim**;
- **<a:developer:928760319021555744> $userTag[728155399655784488] - 728155399655784488**
+$addField[1;**<a:tac:945816748828028938>・Geliştiricim**;
+> **<:developerr:910821172877344778> $userTag[728155399655784488] - 728155399655784488**
 ]
 $footer[1;Komut $userTag Tarafından Kullanıldı;$userAvatar[$authorID]]
 $addButton[1;Davet Et;link;https://discord.com/api/oauth2/authorize?client_id=933330103180623892&scope=bot+applications.commands&permissions=8;no;<:emoji_75:963445326067425370>] 
