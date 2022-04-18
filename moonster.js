d241ac898937b8d3a19a2336e678b69a2cc6aa11
Glitch (jewel-juicy-lyric) <none>
2022-04-18 12:55:53 +0000

    🌯🌓 Checkpoint ./moonster.js:38852640/9

--- moonster.js
+++ moonster.js
@@ -129,7 +129,7 @@ bot.variables({
 });
 
 bot.status({
-  text: "?yardım | ?destek | ?davet | 7/24 Aktif", //buraya durum yazısı
+  text: "?yardım | ?destek | 7/24 Aktif", //buraya durum yazısı
   type: "STREAMING", //buraya oynuyor bölümü PLAYING LISTENING WATCHING STREAMING şeklindede yapabilirsiniz
   status: "online", //buraya status kısmı dnd idle online şeklinde yapabilirsiniz
   time: 12, //buraya ellemeyin
