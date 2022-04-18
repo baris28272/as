c6bd82790397f7f4f2d90ca7eb1c3655ff165591
Glitch (jewel-juicy-lyric) <none>
2022-04-18 21:16:13 +0000

    🍖📙 Checkpoint ./komutlar/istatistik.js:38852640/5

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -26,6 +26,7 @@ $addField[1;**<a:tac:945816748828028938>・Geliştiricim**;
 > **<:developerr:910821172877344778> $userTag[728155399655784488] - 728155399655784488**
 ]
 $footer[1;Komut $userTag Tarafından Kullanıldı;$userAvatar[$authorID]]
+
 $addButton[1;Davet Et;link;https://discord.com/api/oauth2/authorize?client_id=933330103180623892&scope=bot+applications.commands&permissions=8;no;<:emoji_75:963445326067425370>] 
 $addButton[1;Destek;link;https://discord.gg/J8SkJwhYBs;no;<:emoji_75:963445326067425370>]  
 $onlyIf[$getGlobalUserVar[kl;$authorID]!=true;\`$getGlobalUserVar[ksebep;$authorID]\` sebebinden karalistedesiniz.] 
