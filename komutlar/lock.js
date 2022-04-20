8c80ac745317bd66b21b8d5f6164dbe07eab4ab7
Glitch (moonsterbotx21) <none>
2022-04-20 20:15:56 +0000

    🙌⏰ Checkpoint ./komutlar/lock.js:38852640/370

--- komutlar/lock.js
+++ komutlar/lock.js
@@ -1,11 +1,12 @@
 module.exports = {
 name:"lock",
-$if:"v4",
 code:`
 $author[1;$userTag;$userAvatar[$authorID]]
 $color[1;RANDOM]
 $description[1;**$customEmoji[emoji_70] Başarıyla <#$mentionedChannels[1;yes]> İsimli Kanal Kilitlendi.**]
-$footer[1;Komut $userTag Tarafından Kullanıldı;$user
+$footer[1;Komut $userTag Tarafından Kullanıldı;$userAvatar[$authorID]]
+$modifyChannelPerms[$roleID[@everyone];$mentionedChannels[1;yes];-sendmessage]
+$onlyPerms[admin;{newEmbed:{description:**$customEmoji[emoji_71] <@$authorID> Bunu Kullanman İçin \`Yönetici\` İznin Olmalı**}{color:RANDOM}}]
 
   `
   }
