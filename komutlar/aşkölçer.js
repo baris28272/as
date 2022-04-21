1497369e54d7d63d006dc0b437b97a7da7e13661
Glitch (moonsterbotx21) <none>
2022-04-21 21:00:11 +0000

    👨‍👨‍👦🍵 Checkpoint ./komutlar/sunucuavtar.js:38852640/2 ./komutlar/şifre.js:38852640/247 ./komutlar/aşkölçer.js:38852640/1

--- komutlar/aşkölçer.js
+++ komutlar/aşkölçer.js
@@ -3,6 +3,7 @@ module.exports = {
   aliases: "aşk-ölçer",
   code: `
 $onlyIf[$mentioned[1]!=;**$customEmoji[emoji_71] • Birini Etiketle.**]
+
 $author[1;$userTag;$userAvatar[$authorID]
 $color[1;RANDOM]
 $descriprion[1;**$userTag & $userTag[$mentioned[1]] Aranızdaki Aşk %$random[1;100]'dur]
--- komutlar/sunucuavtar.js
+++ komutlar/sunucuavtar.js
@@ -4,7 +4,7 @@ code:`
 $author[1;$serverName Sunucunun Avatarı;$serverIcon]
 $color[1;RANDOM]
 $description[1;$image[1;$serverIcon]]
-$footer[Komut,$userTag Tarafından Kullanıldı;$authorAvatar]
+$footer[1;Komut,$userTag Tarafından Kullanıldı;$authorAvatar]
 $onlyIf[$getGlobalUserVar[kl;$authorID]!=true;\`$getGlobalUserVar[ksebep;$authorID]\` sebebinden karalistedesiniz.] 
 
 `
