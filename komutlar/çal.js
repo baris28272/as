4df718cafd12b502889013c4a8edb1c66e973dfc
Glitch (moonsterbotx21) <none>
2022-04-21 15:30:43 +0000

    💃💫 Checkpoint ./komutlar/çal.js:38852640/90

--- komutlar/çal.js
+++ komutlar/çal.js
@@ -3,9 +3,9 @@ name:"çal",
 code:`
 $author[1;$userTag;$userAvatar[$authorID]]
 $color[1;RANDOM]
-$description[1;**$customEmoji[tatlimisir] <@$authorID> Şuan \`$message[1]\` İsimli Şarkı Çalınıyor**]
-$playTrack[soundcloup;$message[1]]
-$joinVC[$voiceID[$authorID]] 
+$description[1;**$customEmoji[tatlimisir] <@$authorID> Şuan \`$message\` İsimli Şarkı Çalınıyor**]
+$playTrack[Youtube;$message]
+$joinVc[$voiceID[$authorID]]
 
 `
   }
