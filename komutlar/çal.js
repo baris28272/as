4b1e789f4235fa3a1953ed204d3eb7904008c2dc
Glitch (moonsterbotx21) <none>
2022-04-21 15:42:44 +0000

    🚀🐲 Checkpoint ./komutlar/çal.js:38852640/523

--- komutlar/çal.js
+++ komutlar/çal.js
@@ -5,7 +5,23 @@ $author[1;$userTag;$userAvatar[$authorID]]
 $color[1;RANDOM]
 $description[1;**$customEmoji[tatlimisir] <@$authorID> Şuan \`$message\` İsimli Şarkı Çalınıyor**]
 $playTrack[Youtube;$message]
-$joinVc[$voiceID[$authorID]]
 
 `
+  },
+{
+name:"geç",
+code:`
+$author[1;$userTag;$userAvatar[$authorID]]
+$color[1;RANDOM]
+$description[1;**$customEmoji[tatlimisir] <@$authorID> Başarıyla Diğer Şarkıya Geçildi**]
+$skip
+`
+  },
+  {
+  name:"join",
+  code:`
+  $author[1;$userTag;$userAvatar[$authorID]]
+$color[1;RANDOM]
+$description[1;**$customEmoji[tatlimisir] <@$authorID>
+  `
   }
