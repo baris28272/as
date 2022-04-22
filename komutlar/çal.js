4453d1d6376b57a91b40b98213c1c3ba88461104
Glitch (moonsterbotx21) <none>
2022-04-22 05:24:26 +0000

    🏕🍯 Checkpoint ./komutlar/çal.js:38852640/2

--- komutlar/çal.js
+++ komutlar/çal.js
@@ -4,7 +4,7 @@ module.exports = {
 $author[1;$userTag;$userAvatar[$authorID]]
 $color[1;RANDOM]
 $description[1;**$customEmoji[tatlimisir] <@$authorID> Şuan \`$message\` İsimli Şarkı Çalınıyor.**]
-$playTrack[Youtube;$message]
+$playTrack[youtube;$message]
 $onlyIf[$voiceID[$clientID]!=;{newEmbed:{description:**Benim olduğum ses kanalında yoksan kullanamazsın**}{color:RANDOM}}]
 $onlyIf[$voiceID!=;{newEmbed:{description:**Bir ses kanalına girmezsen kullanamazsın**}{color:RANDOM}}]
 $suppressErrors
