3fa33ce8469346f8a2b1e3d9a55a41031b2e117e
Glitch (moonsterbotx21) <none>
2022-04-23 09:34:55 +0000

    ⛩🌓 Checkpoint ./komutlar/mesleksistemi.js:38852640/294

--- komutlar/mesleksistemi.js
+++ komutlar/mesleksistemi.js
@@ -10,7 +10,6 @@ $setGlobalUserVar[meslek;Polis;$authorID]
 $setGlobalUserVar[maaş;20000;$authorID]
 $setGlobalUserVar[para;$sub[$getGlobalUserVar[para;$authorID];200000];$authorID]
 $onlyIf[$getGlobalUserVar[para;$authorID]>200000;{newEmbed:{description:**<@$authorID>, cüzdanında yeterli miktarda para yok.**}{color:RANDOM}}]
-$onlyIf[$getGlobalUserVar[meslek;$authorID]!=Bulunmuyor;{newEmbed:{description:**$customEmoji[emoji_71] <@$authorID> Zaten Mesleğin Var İstifa Etmek İçin \`?istifa-et\` Yazmalısın**}{color:RANDOM}}]
 $endif
 $if[$toLowercase[$message]==Doktor]
 $author[1;$userTag;$userAvatar[$authorID]]
@@ -18,8 +17,9 @@ $color[1;RANDOM]
 $description[1;**$customEmoji[emoji_70] <@$authorID> Başarıyla Doktor Oldun**]
 $setGlobalUserVar[meslek;Doktor;$authorID]
 $setGlobalUserVar[maaş;25000;$authorID]
-$setGlobalUserVar[para;$sub[$getGlobalUserVar[para;$authorID];200000];$authorID]
+$setGlobalUserVar[para;$sub[$getGlobalUserVar[para;$authorID];150000];$authorID]
 $onlyIf[$getGlobalUserVar[para;$authorID]>150000;{newEmbed:{description:**$customEmoji[emoji_71] <@$authorID> cüzdanında yeterli miktarda para yok.**}{color:RANDOM}}]
+
 $onlyIf[$getGlobalUserVar[meslek;$authorID]!=Bulunmuyor;{newEmbed:{description:**$customEmoji[emoji_71] <@$authorID> Zaten Mesleğin Var İstifa Etmek İçin \`?istifa-et\` Yazmalısın**}{color:RANDOM}}]
 $argsCheck[1;{newEmbed:{description:**$customEmoji[emoji_71] <@$authorID> Komutu Kullanmak İçin \`?meslek-seç Polis/Doktor/Aşcı/Gazeteci\` Yazmalısın**}{color:RANDOM}}]
 `
