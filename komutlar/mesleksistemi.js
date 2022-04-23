076aa8bdb48f05a762a85d09a08293073717720f
Glitch (moonsterbotx21) <none>
2022-04-23 08:36:35 +0000

    🚠💶 Checkpoint ./komutlar/mesleksistemi.js:38852640/444

--- komutlar/mesleksistemi.js
+++ komutlar/mesleksistemi.js
@@ -2,7 +2,11 @@ module.exports = {
 name:"meslek-seç",
 $if:"v4",
 code:`
-$if[$message[1]==polis]
+$if[$message[1]==]
+$author[1;$userTag;$userAvatar[$authorID]]
+$color[1;RANDOM]
+$description[1;**$customEmoji[emoji_71] <@$authorID> Komutu Kullanmak İçin \`?meslek-seç Polis/Doktor/Aşcı/Gazeteci\` Yazmalısın**]
+$if[$toLowercase[$message]==Polis]
 $author[1;$userTag;$userAvatar[$authorID]]
 $color[1;RANDOM]
 $description[1;**$customEmoji[emoji_70] <@$authorID> Başarıyla Polis Oldun**]
@@ -10,6 +14,6 @@ $setGlobalUserVar[meslek;Polis;$authorID]
 $setGlobalUserVar[maaş;20000;$authorID]
 $setGlobalUserVar[para;$sub[$getGlobalUserVar[para;$authorID];200000];$authorID]
 $onlyIf[$getGlobalUserVar[para;$authorID]>200000;**<@$authorID>, cüzdanında yeterli miktarda para yok.**]
-$endif
+
 `
   }
