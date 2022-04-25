31d7da005fd78e9af5ab352bdc1f9a823256ec9f
Glitch (moonsterbotx21) <none>
2022-04-25 09:00:13 +0000

    🏅🏵 Checkpoint ./komutlar/doktorol.js:38852640/878 ./komutlar/mesleksistemi.js:38852640/770

--- komutlar/mesleksistemi.js
+++ komutlar/mesleksistemi.js
@@ -1,8 +1,6 @@
 module.exports = {
-name:"meslek-seç",
-$if:"v4",
+name:"meslek-seç polis",
 code:`
-$if[$toLowercase[$message]==polis]
 $author[1;$userTag;$userAvatar[$authorID]]
 $color[1;RANDOM]
 $description[1;**$customEmoji[emoji_70] <@$authorID> Başarıyla Polis Oldun Ve Maaşın [$abbreviate[$getGlobalUserVar[maaş]]]($getBotInvite[admin]) **]
@@ -11,17 +9,6 @@ $setGlobalUserVar[maaş;20000;$authorID]
 $setGlobalUserVar[para;$sub[$getGlobalUserVar[para;$authorID];200000];$authorID]
 $onlyIf[$getGlobalUserVar[para;$authorID]>200000;{newEmbed:{description:**<@$authorID>, cüzdanında yeterli miktarda para yok.**}{color:RANDOM}}]
 $onlyIf[$getGlobalUserVar[meslek;$authorID]!=Bulunmuyor;{newEmbed:{description:**$customEmoji[emoji_71] <@$authorID> Zaten Mesleğin Var İstifa Etmek İçin \`?istifa-et\` Yazmalısın**}{color:RANDOM}}]
-$endif
-$if[$toLowercase[$message]==doktor]
-$author[1;$userTag;$userAvatar[$authorID]]
-$color[1;RANDOM]
-$description[1;**$customEmoji[emoji_70] <@$authorID> Başarıyla Doktor Oldun**]
-$setGlobalUserVar[meslek;Doktor;$authorID]
-$setGlobalUserVar[maaş;25000;$authorID]
-$setGlobalUserVar[para;$sub[$getGlobalUserVar[para;$authorID];150000];$authorID]
-$onlyIf[$getGlobalUserVar[para;$authorID]>150000;{newEmbed:{description:**$customEmoji[emoji_71] <@$authorID> cüzdanında yeterli miktarda para yok.**}{color:RANDOM}}]
-$onlyIf[$getGlobalUserVar[meslek;$authorID]!=Bulunmuyor;{newEmbed:{description:**$customEmoji[emoji_71] <@$authorID> Zaten Mesleğin Var İstifa Etmek İçin \`?istifa-et\` Yazmalısın**}{color:RANDOM}}]
-$endif
 $argsCheck[1;{newEmbed:{description:**$customEmoji[emoji_71] <@$authorID> Komutu Kullanmak İçin \`?meslek-seç Polis/Doktor/Aşcı/Gazeteci\` Yazmalısın**}{color:RANDOM}}]
 
 `
