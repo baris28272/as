5749f76714f7b8ae33170a80e6003d9b0e13bca6
Glitch (moonsterbotx21) <none>
2022-04-27 12:51:30 +0000

    🌑🍦 Checkpoint ./komutlar/küfürengelaç.js:38852640/1298

--- komutlar/küfürengelaç.js
+++ komutlar/küfürengelaç.js
@@ -1,12 +1,21 @@
 module.exports = {
-  name: "küfür-engel aç",
+  name: "küfür-engel",
+  if:"v4",
   code: `
+$if[$message[1]==aç] 
 $color[1;RANDOM]
 $author[1;$userTag[$authorID];$userAvatar[$authorID]]
 $description[1;**$customEmoji[emoji_70] <@$authorID> Başarıyla Küfür Engel Sistemi Açıldı.**]
 $setServerVar[küfürengell;Açık]
-$onlyPerms[admin;{newEmbed:{description:**<@$authorID> Bunu Kullanmak İçin \`Yönetici\` İznin Olmalı**}{color:RANDOM}}]
-$argsCheck[1;{newEmbed:{author:$userTag:$user{description:**<@$authorID> Bir Prefix Belirtmelisin}{color:RANDOM}}]
-
+$endif
+$if[$message[1]==kapat] 
+$color[1;RANDOM]
+$author[1;$userTag[$authorID];$userAvatar[$authorID]]
+$description[1;**$customEmoji[emoji_70] <@$authorID> Başarıyla Küfür Engel Sistemi Kapatıldı.**]
+$setServerVar[küfürengell;Kapalı]
+$onlyIf[$getServerVar[küfürengell]!=Kapalı;{newEmbed:{author:$userTag:$userAvatar[$authorID]}{description:**$customEmoji[emoji_71] <@$authorID> Küfür Engel Sistemi Zaten Kapalı Açmak İçin **}{color:RANDOM}}]
+$endif
+$onlyPerms[admin;{newEmbed:{author:$userTag:$userAvatar[$authorID]}{description:**$customEmoji[emoji_71 ] <@$authorID> Bunu Kullanmak İçin \`Yönetici\` İznin Olmalı**}{color:RANDOM}}]
+$onlyIf[$checkContains[$toLowercase[$message[1]];aç;kapat]==true;{newEmbed:{author:$userTag:$userAvatar[$authorID]}{description:**$customEmoji[emoji_71] <@$authorID> aç veya kapat Yazmalısın**}{color:RANDOM}}]
 `
 }
