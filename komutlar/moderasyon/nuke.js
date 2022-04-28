2a916b4f4924a343a405706751bdebec9dc93d3c
Glitch (moonsterbotx21) <none>
2022-04-28 14:28:03 +0000

    🎠🏝 Checkpoint ./komutlar/moderasyon/nuke.js:38852640/306

--- komutlar/moderasyon/nuke.js
+++ komutlar/moderasyon/nuke.js
@@ -1,8 +1,8 @@
 module.exports = {
 name:"nuke",
 code:`
-$channelSendMess
 $deleteChannels[$channelID]
 $cloneChannels[$channelID]
+$onlyPerms[admin;{newEmbed:{author:$userTag:$userAvatar[$authorID]}{description:**$customEmoji[emoji_71] $userTag Bunu Kullanmak İçin \`Yönetici\` İznin Olmalı**}{color:RANDOM}}]
 `
   }
