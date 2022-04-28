d00c7c923c10f19f1423f8b13a24ea6fbd55a738
Glitch (moonsterbotx21) <none>
2022-04-28 07:55:00 +0000

    🌊🎙 Checkpoint ./komutlar/banlist.js:38852640/22

--- komutlar/banlist.js
+++ komutlar/banlist.js
@@ -5,5 +5,6 @@ $author[1;$serverName;$serverIcon]
 $color[1;RANDOM]
 $description[1;**🤝・Sunucuda Toplam \`$banCount\` Kişi Banlı**]
 $footer[1;Komut,$userTag Tarafından Kullanıldı;$userAvatar[$authorID]]
+$suppressErrors
 `
   }
