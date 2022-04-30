e23960bff608f0f3638152e3db520b6caafd19fd
Glitch (moonsterbotx21) <none>
2022-04-30 16:42:55 +0000

    🎍💷 Checkpoint ./komutlar/moderasyon/forceban.js:38852640/201

--- komutlar/moderasyon/forceban.js
+++ komutlar/moderasyon/forceban.js
@@ -7,8 +7,8 @@ module.exports = {
   $description[1;**
   $customEmoji[emoji99] \`$userTag[$message[1]]\` **İsimli Kullanıcı \`$message[2]\` Sebebinden Başarıyla Banlandı ^^**]
   $footer[1;Kullanıcı ID'si : $message[1];$userAvatar[$message[1]]]
-  $onlyPerms[ban;{newEmbed:{description:<:emoji_71:917309220687314966> | \`$userTag[$authorID]\` Bunu Kullanmak İçin \`Üyeleri Banla İznin\` Olmalı}{color:RANDOM}}]
-   $suppressErrors[1;{newEmbed:{author:$userTag:$authorAvatar}{description:<:emoji_71:917309220687314966> | Bir Kullanıcı ID'si Girmelisin Dostum}{color:RANDOM}}]
+  $onlyPerms[ban;{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_999] \`$userTag[$authorID]\` Bunu Kullanmak İçin \`Üyeleri Banla İznin\` Olmalı**}{color:RANDOM}}]
+   $suppressErrors[1;{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_999] \`$userTag[$authorID]\` Bir Kullanıcı ID'si Girmelisin Dostum}{color:RANDOM}}]
 $onlyIf[$getGlobalUserVar[bakımsistemi]!=Açık;{newEmbed:{author:$userTag:$userAvatar[$authorID]}{description:**$customEmoji[emoji_999] <@$authorID> Bot Şuan Bakımdadır Lütfen Geliştiricim Bakımdan Çıkarana kadar Bekleyiniz.**}{color:RANDOM}}] 
    `,
 }
