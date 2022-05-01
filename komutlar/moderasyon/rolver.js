ec452045e45fb9e59496d8365de159d82f92f08b
Glitch (moonsterbotx21) <none>
2022-05-01 09:27:20 +0000

    🏨🌦 Checkpoint ./komutlar/moderasyon/rolal.js:38852640/40 ./komutlar/moderasyon/rolver.js:38852640/65 ./komutlar/moderasyon/saas-aç.js:38852640/34 ./komutlar/moderasyon/saas-kapat.js:38852640/89 ./komutlar/moderasyon/sil.js:38852640/285 ./komutlar/moderasyon/ban.js:38852640/4

--- komutlar/moderasyon/rolver.js
+++ komutlar/moderasyon/rolver.js
@@ -6,9 +6,9 @@ $color[1;RANDOM]
 $author[1;$userTag[$authorID];$userAvatar[$authorID]]
 $thumbnail[1;$userAvatar[$mentioned[1]]]
 $description[1;
-**$customEmoji[emoji_70] | \`$userTag[$authorID]\` Başarıyla \`$userTag[$mentioned[1]]\` Kişisine <@&$mentionedRoles[1]> Rolünü Verdim**
+**$customEmoji[emoji99] \`$userTag[$authorID]\` Başarıyla \`$userTag[$mentioned[1]]\` Kişisine <@&$mentionedRoles[1]> Rolünü Verdim**
 ]
-$onlyPerms[manageroles;{newEmbed:{description:**$customEmoji[emoji_71] | Bu Komutu Kullanmak İçin \`Rolleri Yönet\` İznin Olmalı**}{color:RANDOM}}]
+$onlyPerms[manageroles;{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_999] | Bu Komutu Kullanmak İçin \`Rolleri Yönet\` İznin Olmalı**}{color:RANDOM}}]
 $suppressErrors
 $onlyIf[$getGlobalUserVar[bakımsistemi]!=Açık;{newEmbed:{author:$userTag:$userAvatar[$authorID]}{description:**$customEmoji[emoji_999] <@$authorID> Bot Şuan Bakımdadır Lütfen Geliştiricim Bakımdan Çıkarana kadar Bekleyiniz.**}{color:RANDOM}}] 
 `,
--- komutlar/moderasyon/saas-aç.js
+++ komutlar/moderasyon/saas-aç.js
@@ -3,10 +3,10 @@ name:"saas-aç",
 code:`
 $author[1;$userTag;$userAvatar[$authorID]]
 $color[1;RANDOM]
-$description[1;**$customEmoji[emoji_70]・Başarıyla Sa As Sistemini Aktif Ettin**]
+$description[1;**$customEmoji[emoji99] <@$authorID> Başarıyla Sa As Sistemini Aktif Ettin**]
 $setServerVar[saas;açık]
-$onlyIf[$getServerVar[saas]!=açık;{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_71] <@$authorID>,Selam Sistemi Zaten Aktif Kapatmak İçin \`?saas-kapat\`**}{color:RANDOM}}]
-$onlyPerms[managemessages;{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_71] Bu Komutu Kullanmak İçin \`Mesajları Yönet\` Yetkin Olmalı**}{color:RANDOM}}]
+$onlyIf[$getServerVar[saas]!=açık;{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_999] <@$authorID>,Selam Sistemi Zaten Aktif Kapatmak İçin \`?saas-kapat\`**}{color:RANDOM}}]
+$onlyPerms[managemessages;{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_999] Bu Komutu Kullanmak İçin \`Mesajları Yönet\` Yetkin Olmalı**}{color:RANDOM}}]
 $onlyIf[$getGlobalUserVar[bakımsistemi]!=Açık;{newEmbed:{author:$userTag:$userAvatar[$authorID]}{description:**$customEmoji[emoji_999] <@$authorID> Bot Şuan Bakımdadır Lütfen Geliştiricim Bakımdan Çıkarana kadar Bekleyiniz.**}{color:RANDOM}}] 
 $suppressErrors
 `
