360ddad76de8bb36c27e08fc8fcda71bad57fbda
Glitch (moonsterbotx21) <none>
2022-05-01 19:40:48 +0000

    🌺💘 Checkpoint ./komutlar/kayıtsistemi/kayıtsızrol.js:38852640/3

--- komutlar/kayıtsistemi/kayıtsızrol.js
+++ komutlar/kayıtsistemi/kayıtsızrol.js
@@ -6,7 +6,7 @@ module.exports = {
   $description[1;**$customEmoji[emoji99] <@$authorID> Kayıtsız Rolü Başarıyla <@&$mentionedRoles[1]> Olarak Ayarlandı**]
   $onlyPerms[admin;{newEmbed:{author:$userTag:$userAvatar[$authorID]}{description:**$customEmoji[emoji_999] <@$authorID> Bu Komutu Kullanmak İçin \`Yönetici\` İznin Olmalı**}{color:RANDOM}}]
   $argsCheck[1;{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_999] <@$authorID> Bir Kanal Etiketlemelisin**}{color:RANDOM}}]
-  $setServerVar[kayıtrol;$mentionedRoles[1]]
+  $setServerVar[kayıtsızrol;$mentionedRoles[1]]
   
   `
   }
