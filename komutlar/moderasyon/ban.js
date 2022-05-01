a8f28d487f2d4d8268443fa22b5dee18d3586112
Glitch (moonsterbotx21) <none>
2022-05-01 15:24:33 +0000

    ⏳🌹 Checkpoint ./komutlar/moderasyon/ban.js:38852640/1627

--- komutlar/moderasyon/ban.js
+++ komutlar/moderasyon/ban.js
@@ -5,14 +5,7 @@ $ban[$mentioned[1];$guildID;0;$noMentionMessage]
 $author[1;$userTag[$mentioned[1]];$userAvatar[$mentioned[1]]]
 $description[1;$customEmoji[emoji99] \`$userTag[$mentioned[1]]\` İsimli Kullanıcı \`$noMentionMessage\` Sebebi İle Banlandı ^^]
 $color[1;RANDOM]
-$onlyIf[$highestRole[$message[1]]!=$highestRole[$authorID];{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_999] Aynı En Yüksek Role Sahip Birini Yasaklayamazsınız**}{color:RANDOM}}]
-$onlyIf[$rolePosition[$highestRole[$authorID]]<$rolePosition[$highestRole[$mentioned[1]]];{newEmbed:{author:$userTag:$authorAvatar}{description:*$customEmoji[emoji_999] Senden Daha Yüksek Bir Role Sahip Birini Yasaklayamazsın**}{color:RANDOM}}]
-$onlyIf[$rolePosition[$highestRole[$clientID]]<$rolePosition[$highestRole[$mentioned[1]]];{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_999] Benden Daha Yüksek Rolü Olan Birini Banlayamam**}{color:RANDOM}}] 
-$onlyIf[$highestRole[$mentioned[1]]!=$highestRole[$clientID];{newEmbed:{author:$userTag;$authorAvatar}{description:**$customEmoji[emoji_999] Benimle Aynı Yüksek Role Sahip Birini Yasaklayamam**}{color:RANDOM}}]
-$onlyIf[$mentioned[1]!=$ownerID;{newEmbed:{author:$userTag;$authorAvatar}{description:**$customEmoji[emoji_999] Sunucu Sahibini Yasaklayamazsınız**}{color:RANDOM}}]
-$onlyIf[$mentioned[1]!=$authorID;{newEmbed:{author:$userTag;$authorAvatar}{description:**$customEmoji[emoji_999] \`$userTag[$authorID]\` Birini Etiketlemelisin**}{color:RANDOM}}] 
-$onlyIf[$mentioned[1]!=$clientID;:{newEmbed:{author:$userTag;$authorAvatar}{description:**$customEmoji[emoji_999] Kendimi Yasaklayamam**}{color:RANDOM}}]
-$onlyIf[$hasPerms[$guildID;$authorID;ban]==true;{newEmbed:{author:$userTag;$authorAvatar}{description:**$customEmoji[emoji_999] Bu Komutu Kullanmak İçin \`Üyeleri Banla\` Yetkin Olmalı**}{color:RANDOM}}] 
+
 $onlyIf[$getGlobalUserVar[bakımsistemi]!=Açık;{newEmbed:{author:$userTag:$userAvatar[$authorID]}{description:**$customEmoji[emoji_999] <@$authorID> Bot Şuan Bakımdadır Lütfen Geliştiricim Bakımdan Çıkarana kadar Bekleyiniz.**}{color:RANDOM}}] 
 `
 };
