636d715c4d68de879988be69f306e21e0f2c107d
Glitch (moonsterbotx21) <none>
2022-05-01 16:00:33 +0000

    🕌🐣 Checkpoint ./komutlar/moderasyon/ban.js:38852640/211

--- komutlar/moderasyon/ban.js
+++ komutlar/moderasyon/ban.js
@@ -3,9 +3,9 @@ module.exports = {
   code: `
 $ban[$mentioned[1];$guildID;0;$noMentionMessage]
 $author[1;$userTag[$mentioned[1]];$userAvatar[$mentioned[1]]]
-$description[1;$customEmoji[emoji99] \`$userTag[$mentioned[1]]\` İsimli Kullanıcı \`$noMentionMessage\` Sebebi İle Banlandı ^^]
+$description[1;**$customEmoji[emoji99] \`$userTag[$mentioned[1]]\` İsimli Kullanıcı \`$noMentionMessage\` Sebebi İle Banlandı ^^**]
 $color[1;RANDOM]
-
+$argsCheck[1;{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_999] <@$authorID> Birisini Etiketlemelisin**}{color:RANDOM}}]
 $onlyIf[$getGlobalUserVar[bakımsistemi]!=Açık;{newEmbed:{author:$userTag:$userAvatar[$authorID]}{description:**$customEmoji[emoji_999] <@$authorID> Bot Şuan Bakımdadır Lütfen Geliştiricim Bakımdan Çıkarana kadar Bekleyiniz.**}{color:RANDOM}}] 
 `
 };
