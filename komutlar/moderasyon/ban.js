c1d5e63cbc73fdd43b3e38445ed9c5131655fa54
Glitch (moonsterbotx21) <none>
2022-05-01 09:39:21 +0000

    🚙🐸 Checkpoint ./komutlar/moderasyon/ban.js:38852640/2871

--- komutlar/moderasyon/ban.js
+++ komutlar/moderasyon/ban.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "ban",
   code: `
+$ban[$message[1];$guildID;0;sebeb]
 $author[1;$userTag[$mentioned[1]];$userAvatar[$mentioned[1]]]
-$ban[$mentioned[1];$guildID;1;$noMentionMessage]
 $description[1;$customEmoji[emoji99] \`$userTag[$mentioned[1]]\` İsimli Kullanıcı \`$noMentionMessage\` Sebebi İle Banlandı ^^]
 $color[1;RANDOM]
 $onlyIf[$highestRole[$mentioned[1]]!=$highestRole[$authorID];{newEmbed:{author:$userTag:$authorAvatar}{description:**$customEmoji[emoji_999] Aynı En Yüksek Role Sahip Birini Yasaklayamazsınız**}{color:RANDOM}}]
@@ -16,7 +16,5 @@ $onlyIf[$hasPerms[$guildID;$authorID;ban]==true;{newEmbed:{author:$userTag;$auth
 $onlyBotPerms[ban;{newEmbed:{author:$userTag;$authorAvatar}{description:**$customEmoji[emoji_999] <@$authorID> Bu Komutu Kullanmak İçin Bana \`Üyeleri Banla\` Yetkisi Vermelisin ^^**}{color:RANDOM}}]
 $suppressErrors[1;{newEmbed:{author:$userTag;$authorAvatar}{description:**$customEmoji[emoji_999] <@$authorID> Bütün Gün Kimi Banlayacağımı Düşünücem ^^**}{color:RANDOM}}]
 $onlyIf[$getGlobalUserVar[bakımsistemi]!=Açık;{newEmbed:{author:$userTag:$userAvatar[$authorID]}{description:**$customEmoji[emoji_999] <@$authorID> Bot Şuan Bakımdadır Lütfen Geliştiricim Bakımdan Çıkarana kadar Bekleyiniz.**}{color:RANDOM}}] 
-
-
 `
 };
