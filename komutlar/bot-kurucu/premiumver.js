ea4c983828cbb4bc3399b5109059a3a593ea3d25
Glitch (moonsterbotx21) <none>
2022-05-15 17:26:47 +0000

    🌾✋ Checkpoint

--- komutlar/bot-kurucu/premiumver.js
+++ komutlar/bot-kurucu/premiumver.js
@@ -1,7 +1,7 @@
 module.exports = {
 name:"premium-ver",
 code:`
-$sendMessage[**$customEmoji[emoji99] \`$userTag[$get[y]]\` Kişisinin \`$get[s]\` Süreli Premiumun Süresi Bittiği İçin Premium'u Aldım**;no]
+$channelSendMessage[975444593581649981;**$customEmoji[emoji99] \`$userTag[$get[y]]\` Kişisinin \`$get[s]\` Süreli Premiumun Süresi Bittiği İçin Premium'u Aldım**;no]
 $setGlobalUserVar[premium;Bulunmuyor;$get[y]]
 $wait[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$get[s]; ;];saniye;s];dakika;m];saat;h];gün;d]]
 $sendMessage[**$customEmoji[emoji99] \`$userTag[$get[y]]\` Kişisine Başarıyla \`$get[s]\` Süresine Kadar Premium Verildi**;no]
@@ -9,7 +9,7 @@ $setGlobalUserVar[premium;Bulunuyor;$get[y]]
 $let[s;$messageSlice[1]]
 $let[k;$authorID]
 $let[y;$mentioned[1]]
-$onlyIf[$checkContains[$messageSlice[1];saniye;dakika;saat;gün]!=false;**$customEmoji[emoji_999] <@$authorID> Bir süre girin. \`1 Saniye | 1 Dakika | 1 Saat | 1 gün\`**]
+$onlyIf[$checkContains[$messageSlice[1];Saniye;Dakika;Saat;Gün]!=false;**$customEmoji[emoji_999] <@$authorID> Bir süre girin. \`1 Saniye | 1 Dakika | 1 Saat | 1 gün\`**]
 $suppressErrors[1;**$customEmoji[emoji_999] <@$authorID> Birini Etiketleyin ve bir Süre Belirt \`1 Saniye | 1 Dakika | 1 Saat | 1 gün\`**]
 $onlyIf[$messageSlice[1]!=;**$customEmoji[emoji_999] <@$authorID> Bir süre girin. \`1 Saniye | 1 Dakika | 1 Saat | 1 gün\`**]
 $onlyIf[$isBot[$mentioned[1]]!=true;**$customEmoji[emoji_999] <@$authorID> Bir bota premium veremezsin**]
