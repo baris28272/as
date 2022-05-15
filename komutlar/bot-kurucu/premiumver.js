c959b4e5d0ee310b3f070f178ed094060cf031a6
Glitch (moonsterbotx21) <none>
2022-05-15 16:42:14 +0000

    🏓💴 Checkpoint ./komutlar/bot-kurucu/premiumver.js:38852640/601

--- komutlar/bot-kurucu/premiumver.js
+++ komutlar/bot-kurucu/premiumver.js
@@ -1,11 +1,11 @@
 module.exports = {
 name:"premium-ver",
 code:`
-$author[1;$userTag;$authorAvatar]
-$color[1;RANDOM]
-$description[1;**$customEmoji[emoji99] \`$userTag[$mentioned[1]]\` Kişisine \`$userTag\` Tarafından Premium Verildi**]
-$setGlobalUserVar[premium;Bulunuyor;$mentioned[1]]
+$sendMessage[**$customEmoji[emoji99] \`$userTag[$get[y]]\` Kişisine Başarıyla \`$get[s]\` Süresine Kadar Premium Verildi**;no]
+$setGlobalUserVar[premium;Bulunuyor;$get[y]]
 $wait[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$get[s]; ;];saniye;s];dakika;m];saat;h];gün;d]]
-$onlyIf[$getGlobalUserVar[premium;$mentioned[1]]=!Bulunuyor;{newEmbed:{author:$userTag:$author
+$let[s;$messageSlice[1]]
+$let[k;$authorID]
+$let[y;$mentioned[1]]
 `
   }
