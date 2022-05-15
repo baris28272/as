e17d02528ba24761c3dc9211dd1c6c148cd5ac99
Glitch (moonsterbotx21) <none>
2022-05-15 16:30:58 +0000

    🌲😚 Checkpoint ./komutlar/bot-kurucu/premiumver.js:38852640/125

--- komutlar/bot-kurucu/premiumver.js
+++ komutlar/bot-kurucu/premiumver.js
@@ -5,6 +5,7 @@ $author[1;$userTag;$authorAvatar]
 $color[1;RANDOM]
 $description[1;**$customEmoji[emoji99] \`$userTag[$mentioned[1]]\` Kişisine \`$userTag\` Tarafından Premium Verildi**]
 $setGlobalUserVar[premium;Bulunuyor;$mentioned[1]]
+$wait[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$get[s]; ;];saniye;s];dakika;m];saat;h];gün;d]]
 $onlyIf[$getGlobalUserVar[premium;$mentioned[1]]=!Bulunuyor;{newEmbed:{author:$userTag:$author
 `
   }
