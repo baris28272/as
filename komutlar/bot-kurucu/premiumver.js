29ebaa9827ad079debab2416989ff1ac99285a34
Glitch (moonsterbotx21) <none>
2022-05-15 16:54:14 +0000

    🕍🔨 Checkpoint ./komutlar/bot-kurucu/premiumver.js:38852640/1068

--- komutlar/bot-kurucu/premiumver.js
+++ komutlar/bot-kurucu/premiumver.js
@@ -1,9 +1,11 @@
 module.exports = {
 name:"premium-ver",
 code:`
+$sendMessage[**$customEmoji[emoji99] \`$userTag[$get[y]]\` Kişisinin \`$get[s]\` Süreli Premiumun Süresi Bittiği İçin Premium'u Aldım**;no]
+$setGlobalUserVar[premium;Bulunmuyor;$get[y]]
+$wait[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$get[s]; ;];saniye;s];dakika;m];saat;h];gün;d]]
 $sendMessage[**$customEmoji[emoji99] \`$userTag[$get[y]]\` Kişisine Başarıyla \`$get[s]\` Süresine Kadar Premium Verildi**;no]
 $setGlobalUserVar[premium;Bulunuyor;$get[y]]
-$wait[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$get[s]; ;];saniye;s];dakika;m];saat;h];gün;d]]
 $let[s;$messageSlice[1]]
 $let[k;$authorID]
 $let[y;$mentioned[1]]
