d20e554ae3fbccec7de5683859497182113d21fd
Glitch (westabot21xx) <none>
2022-05-26 08:31:14 +0000

    🎂🌜 Checkpoint ./database/main/main_scheme_1.sql.js:38852640/6

--- komutlar/bot-kurucu/lisans.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "lisans",
-  code: `
-  Bu Komut \`$serverName\` Sunucusuna Aittir Çalınması Ve Paylaşılması Kesinlikle Yasaktır.
- $onlyForIDs[$botOwnerID;728155399655784488;]
- $deletecommand
- `,
-}
