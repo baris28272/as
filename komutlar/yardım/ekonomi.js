1e81436d662482693dc77f4885df651c7108cf82
Glitch (westabot21xx) <none>
2022-05-26 11:21:37 +0000

    💺📻 Checkpoint ./komutlar/yardım/ekonomi.js:38852640/512

--- komutlar/yardım/ekonomi.js
+++ komutlar/yardım/ekonomi.js
@@ -2,5 +2,11 @@ module.exports = {
 name:"ekonomi",
 code:`
 $author[1;$userTag;$authorAvatar]
+$color[1;RANDOM]
+$description[1;**
+<:gift:968571121458872370> | \`$getServerVar[prefix]param\` - Etiketlediğiniz kişinin veya kendinizin parasına bakarsınız.
+<:gift:968571121458872370> | \`$getServerVar[prefix]günlük\` - Günlük paranızı alırsınız.
+<:gift:968571121458872370> | \`$getServerVar[prefix]çalış\` - Çalışarak para kazanırsınız.
+
 `
   }
