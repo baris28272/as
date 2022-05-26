7d9b44f2601a7bcc65d8043f96ae91ff003700a4
Glitch (westabot21xx) <none>
2022-05-26 11:33:28 +0000

    👘📖 Checkpoint ./komutlar/yardım/ekonomi.js:38852640/1129

--- komutlar/yardım/ekonomi.js
+++ komutlar/yardım/ekonomi.js
@@ -7,6 +7,14 @@ $description[1;**
 <:gift:968571121458872370> | \`$getServerVar[prefix]param\` - Etiketlediğiniz kişinin veya kendinizin parasına bakarsınız.
 <:gift:968571121458872370> | \`$getServerVar[prefix]günlük\` - Günlük paranızı alırsınız.
 <:gift:968571121458872370> | \`$getServerVar[prefix]çalış\` - Çalışarak para kazanırsınız.
-
+<:gift:968571121458872370> | \`$getServerVar[prefix]kazı-kazan\` -  Kazı kazan oynayarak para kazanırsınız.
+<:gift:968571121458872370> | \`$getServerVar[prefix]balık-tut\` - Balık tutarak para kazanırsınız.
+<:gift:968571121458872370> | \`$getServerVar[prefix]kasa-al\` - Kasa alırsınız.
+<:gift:968571121458872370> | \`$getServerVar[prefix]kasa-aç\` - Aldığınız kasayı açarsınız.
+<:gift:968571121458872370> | \`$getServerVar[prefix]para-çek\` - Bankadan para çekersiniz.
+<:gift:968571121458872370> | \`$getServerVar[prefix]para-yatır\` - Bankaya para yatırırsınız.
+<:gift:968571121458872370> | \`$getServerVar[prefix]para-çal\` - Etiketlediğiniz kişiyi soyarsınız.
+<:gift:968571121458872370> | \`$getServerVar[prefix]pasif-aç\` - Pasif modu açarsınız.
+<:gift:968571121458872370> | \`$getServerVar[prefix]şanslı-sayım\` - Şanslı sayım ile para kazanırsınız.**
 `
   }
