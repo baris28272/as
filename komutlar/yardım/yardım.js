14dff5e3856b2f7e8e895b1e0d2646c39db614a8
Glitch (westabot21xx) <none>
2022-05-28 13:54:42 +0000

    😎🌃 Checkpoint ./komutlar/yardım/yardım.js:38852640/1

--- komutlar/yardım/yardım.js
+++ komutlar/yardım/yardım.js
@@ -4,6 +4,7 @@ code:`
 $author[1;$userTag;$authorAvatar]
 $color[1;RANDOM]
 $description[1;**$customEmoji[tatlimisir] Merhaba ben \`$username[$clientID]\` komutlarımı görmek için menülere bakarak komutlarıma ulaşabilirsin.**
+
 **<:boteglence:968593132453261322> | \`$getServerVar[prefix]kullanıcı\` - Kullanıcı komutlarına bakarsınız.**
 **<:certificatemod:945700629278498847> | \`$getServerVar[prefix]yetkili\` - Yetkili komutlarına bakarsınız.**
 **<:emoji_50:927275482104336384> | \`$getServerVar[prefix]sunucu\` - Sunucu komutlarına bakarsınız.**
