c2fd5a2d6c4badb4b257f98e1b4bdff3c6735699
Glitch (westabot21xx) <none>
2022-05-29 21:01:37 +0000

    🍶🚿 Checkpoint ./komutlar/ekonomi/para-gönder.js:38852640/954 ./komutlar/yardım/yardım.js:38852640/144

--- komutlar/ekonomi/para-gönder.js
+++ komutlar/ekonomi/para-gönder.js
@@ -1,6 +1,12 @@
 module.exports = {
 name:"para-gönder",
 code:`
-$author[1
-`
-  }
+$author[1;$userTag;$authorAvatar]
+$color[1;RANDOM]
+$description[1;**$customEmoji[emoji99] <@$authorID> tarafında <@$mentioned[1]> kişisine \`$noMentionMessage\` miktarında para yolladı.**]
+$setGlobalUserVar[para;$sum[$getGlobalUserVar[para;$mentioned[1]];$noMentionMessage];$mentioned[1]]
+$setGlobalUserVar[para;$sub[$getGlobalUserVar[para;$authorID];$noMentionMessage];$authorID]
+$onlyIf[$mentioned[1]!=$authorID;**$customEmoji[emoji_999] <@$authorID>, kendine para gönderemezsin.**]
+$onlyIf[$noMentionMessage<$getGlobalUserVar[para;$authorID];**$customEmoji[emoji_999] <@$authorID>, o kadar paran yok.**]
+$onlyIf[$noMentionMessage<200001;**$customEmoji[emoji_999] <@$authorID>,  200.000 üstü sayı giremezsin.**]  
+`}
