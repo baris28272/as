a011113886ffb9e07d4ef35b1b61e43fbfb1997b
Glitch (westabot21xx) <none>
2022-05-29 19:26:30 +0000

    🚘🍿 Checkpoint ./komutlar/ekonomi/paraçal.js:38852640/2 ./komutlar/ekonomi/pasif-kapat.js:38852640/368 ./komutlar/yardım/yardım.js:38852640/632

--- komutlar/ekonomi/paraçal.js
+++ komutlar/ekonomi/paraçal.js
@@ -8,7 +8,7 @@ $setGlobalUserVar[para;$sum[$getGlobalUserVar[para;$mentioned[1]];$getGlobalUser
 $setGlobalUserVar[para;0;$mentioned[1]]
 $argsCheck[1;**$customEmoji[emoji_999] <@$authorID>, soyacağın kişiyi etiketlemelisin.**]
 $onlyIf[$getGlobalUserVar[para;$mentioned[1]]>0;**$customEmoji[emoji_999] <@$authorID>, etiketlediğin kişi de para yok.**]  
-$onlyIf[$getGlobalUserVar[pasif;$authorID]!=kapalı;**$customEmoji[emoji_999] @$authorID>, etiketlediğin kişinin pasifi açık soyamazsın.** ] 
-$onlyIf[$getGlobalUserVar[pasif;$authorID]!=açık;**$customEmoji[emoji_999] @$authorID>, pasif modun açık ondan birini soyamazsın.** ] 
+$onlyIf[$getGlobalUserVar[pasif;$authorID]!=kapalı;**$customEmoji[emoji_999] <@$authorID>, etiketlediğin kişinin pasifi açık soyamazsın.** ] 
+$onlyIf[$getGlobalUserVar[pasif;$authorID]!=açık;**$customEmoji[emoji_999] <@$authorID>, pasif modun açık ondan birini soyamazsın.** ] 
 `
   }
--- komutlar/yardım/yardım.js
+++ komutlar/yardım/yardım.js
@@ -3,14 +3,12 @@ name:"yardım",
 code:`
 $author[1;$userTag;$authorAvatar]
 $color[1;RANDOM]
-$description[1;**$customEmoji[tatlimisir] Merhaba ben \`$username[$clientID]\` komutlarımı görmek için menülere bakarak komutlarıma ulaşabilirsin.**
+$description[1;**$customEmoji[tatlimisir] Merhaba ben \`$username[$clientID]\` komutlarımı görmek için menülere bakarak komutlarıma ulaşabilirsin.**]
 
-**<:boteglence:968593132453261322> | \`$getServerVar[prefix]kullanıcı\` - Kullanıcı komutlarına bakarsınız.**
-**<:certificatemod:945700629278498847> | \`$getServerVar[prefix]yetkili\` - Yetkili komutlarına bakarsınız.**
-**<:emoji_50:927275482104336384> | \`$getServerVar[prefix]sunucu\` - Sunucu komutlarına bakarsınız.**
-**<:gift:968571121458872370> | \`$getServerVar[prefix]ekonomi\` - Ekonomi komutlarına bakarsınız.**
-**<:dMaviTac:979338397065838612> | \`$getServerVar[prefix]kurucu\` - Botun kurucusuna özel komutlara bakarsınız.**
+$addField[1;**Ekonomi**;
+\` deneme\`
+]
 
-**Not : Öneri veya Destek almak için \`$getServerVar[prefix]destek\` & \`$getServerVar[prefix]öneri\` yazarak önerinizi veya şikayetinizi bildirebilirsiniz.**
+$addField[1;**Not**;**Not : Öneri veya Destek almak için \`$getServerVar[prefix]destek\` & \`$getServerVar[prefix]öneri\` yazarak önerinizi veya şikayetinizi bildirebilirsiniz.**]
 `
   }
