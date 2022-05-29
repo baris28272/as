6f37c4d8f2976e6771f8f6d80e3afdc088ab35dc
Glitch (westabot21xx) <none>
2022-05-29 20:14:30 +0000

    🐔🎉 Checkpoint ./komutlar/yetkili/kick.js:38852640/164 ./komutlar/yetkili/ban.js:38852640/131 ./komutlar/yetkili/otorol-sistemi aç.js:38852640/522

--- komutlar/yetkili/ban.js
+++ komutlar/yetkili/ban.js
@@ -6,6 +6,8 @@ $color[1;RANDOM]
 $thumbnail[1;$serverIcon]
 $author[1;$userTag;$authorAvatar]
 $description[1;**$customEmoji[emoji99] \`$username[$mentioned[1]]\` adlı kullanıcı \`$noMentionMessage\` sebebiyle başarıyla sunucudan yasaklandı.**]
+$argsCheck[>1;**$customEmoji[emoji_999] <@$authorID> Bir kişiyi etiketlemelisin ve sebep girmelisin**]
 $onlyPerms[ban;**$customEmoji[emoji_999] <@$authorID> Bu komutu sadece Üyeleri \`Yasakla iznine sahip\` olan kullanıcılar kullanabilir.**]
+$suppressErrors
 `
 }  
--- komutlar/yetkili/kick.js
+++ komutlar/yetkili/kick.js
@@ -6,6 +6,8 @@ $color[1;RANDOM]
 $thumbnail[1;$serverIcon]
 $author[1;$userTag;$authorAvatar]
 $description[1;**$customEmoji[emoji99] \`$username[$mentioned[1]]\` adlı kullanıcı \`$noMentionMessage\` sebebiyle başarıyla sunucudan atıldı.**]
+$argsCheck[>1;**$customEmoji[emoji_999] <@$authorID> Bir kişiyi etiketlemelisin ve sebep girmelisin**]
 $onlyPerms[kick;**$customEmoji[emoji_999] <@$authorID> Bu komutu sadece \`Üyeleri At iznine\` sahip olan kullanıcılar kullanabilir.**]
-
+$suppressErrors
+`
 }  
