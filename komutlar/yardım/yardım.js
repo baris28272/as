81e3a5838b761e1b38cc152c00b08b7648c876dc
Glitch (westabot21xx) <none>
2022-05-29 19:38:30 +0000

    🚎⚾️ Checkpoint ./komutlar/yardım/yardım.js:38852640/676

--- komutlar/yardım/yardım.js
+++ komutlar/yardım/yardım.js
@@ -3,12 +3,16 @@ name:"yardım",
 code:`
 $author[1;$userTag;$authorAvatar]
 $color[1;RANDOM]
+$addField[1;**<:boteglence:968593132453261322>・Not**;**Not : Öneri veya Destek almak için \`$getServerVar[prefix]destek\` & \`$getServerVar[prefix]öneri\` yazarak önerinizi veya şikayetinizi bildirebilirsiniz.**]
 $description[1;**$customEmoji[tatlimisir] Merhaba ben \`$username[$clientID]\` komutlarımı görmek için menülere bakarak komutlarıma ulaşabilirsin.**]
 
-$addField[1;**Ekonomi**;
-\` deneme\`
+$addField[1;**<:Blue_Gift:970360659248295966>・Ekonomi**;
+\` ?param / ?günlük / ?çalış / ?kazı-kazan / ?balık-tut / ?kasa-al / ?kasa-aç / ?para-yatır / ?para-çek / ?para-çal / ?pasif-aç / ?pasif-kapat / ?şanslı sayım
+\`
+
+$addField[1;**Yetkili**;
+
 ]
 
-$addField[1;**Not**;**Not : Öneri veya Destek almak için \`$getServerVar[prefix]destek\` & \`$getServerVar[prefix]öneri\` yazarak önerinizi veya şikayetinizi bildirebilirsiniz.**]
 `
   }
