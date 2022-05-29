703ec3f2eb662f27ed61eadce0b2871f3074a19e
Glitch (westabot21xx) <none>
2022-05-29 20:02:31 +0000

    🙅😼 Checkpoint ./komutlar/yetkili/ban.js:38852640/352 ./komutlar/yetkili/kick.js:38852640/195

--- komutlar/yetkili/kick.js
+++ komutlar/yetkili/kick.js
@@ -1,12 +1,11 @@
 module.exports = {
   name: "kick",
   code: `
-$kick[$mentioned[1];$guildID;7;$noMentionMessage]
+$kick[$mentioned[1];$guildID;0;$noMentionMessage]
 $color[1;RANDOM]
 $thumbnail[1;$serverIcon]
 $author[1;$userTag;$authorAvatar]
-$description[1;**$customEmoji[emoji99] \`$username[$mentioned[1]]#$discriminator[$mentioned[1]\` adlı kullanıcı başarıyla sunucudan atıldı.**]
+$description[1;**$customEmoji[emoji99] \`$username[$mentioned[1]]\` adlı kullanıcı \`$noMentionMessage\` sebebiyle başarıyla sunucudan atıldı.**]
 $onlyPerms[kick;**$customEmoji[emoji_999] <@$authorID> Bu komutu sadece \`Üyeleri At iznine\` sahip olan kullanıcılar kullanabilir.**]
-$argsCheck[1;**$customEmoji[emoji_999] <@$authorID> Sunucudan atılacak kişiyi etiketlemelisin.**]
-$suppressErrors`
+
 }  
