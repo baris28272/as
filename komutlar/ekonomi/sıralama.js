b4919453c569b4fabc1a91687907c82a695713c3
Glitch (westabot21xx) <none>
2022-05-29 20:38:31 +0000

    👖🐴 Checkpoint ./komutlar/ekonomi/paraçal.js:38852640/5 ./komutlar/ekonomi/sıralama.js:38852640/434 ./komutlar/yardım/yardım.js:38852640/19

---
+++ komutlar/ekonomi/sıralama.js
@@ -0,0 +1,9 @@
+module.exports = {
+name: "para-sıralaması",
+code: `
+$color[1;RANDOM]
+$author[1;$userTag;$authorAvatar]
+$description[1;**$globalUserLeaderboard[para;asc]**]
+
+`
+}  
--- komutlar/yardım/yardım.js
+++ komutlar/yardım/yardım.js
@@ -7,7 +7,7 @@ $addField[1;**<:boteglence:968593132453261322>・Not**;**Not : Öneri veya Deste
 $description[1;**$customEmoji[tatlimisir] Merhaba ben \`$username[$clientID]\` komutlarımı görmek için menülere bakarak komutlarıma ulaşabilirsin.**]
 
 $addField[1;**<:Blue_Gift:970360659248295966>・Ekonomi**;
-\` ?param / ?günlük / ?çalış / ?kazı-kazan / ?balık-tut / ?kasa-al / ?kasa-aç / ?para-yatır / ?para-çek / ?para-çal / ?pasif-aç / ?pasif-kapat / ?şanslı sayım
+\` ?param / ?günlük / ?çalış / ?kazı-kazan / ?balık-tut / ?kasa-al / ?kasa-aç / ?para-yatır / ?para-çek / ?para-çal / ?pasif-aç / ?pasif-kapat / ?şanslı sayım / ?para-sıralaması
 \`
 $addField[1;**<:admin:977852980990726154>・Yetkili**;
 \` ?ban / ?ban-say / ?kick / ?nuke / ?sil / ?hgbb aç / ?hgbb kapat / ?otorol aç / ?otorol kapat
