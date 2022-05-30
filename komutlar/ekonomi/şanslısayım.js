6fdb74e3753f843a13dd780e0aade6fd5b56b7b0
Glitch (westabot21xx) <none>
2022-05-30 07:04:27 +0000

    ✈️🏉 Checkpoint ./komutlar/yardım/yardım.js:38852640/58 ./komutlar/ekonomi/şanslısayım.js:38852640/5

--- komutlar/ekonomi/şanslısayım.js
+++ komutlar/ekonomi/şanslısayım.js
@@ -3,7 +3,7 @@ name: "şanslı-sayı oyunu",
 code: `
 $color[1;RANDOM]
 $author[1;$userTag;$authorAvatar]
-$description[1;**$customEmoji[emoji99] <@$authorID>, şanslı sayın  \`$random[1;100]\` olduğu için \`$random[1;100]\` miktar para kazandın.**]
+$description[1;**$customEmoji[emoji99] <@$authorID>, şanslı sayın \`$random[1;100]\` olduğu için \`$random[1;100]\` miktar para kazandın.**]
 $setGlobalUserVar[para;$sum[$getGlobalUserVar[para;$authorID];$random[1;100]];$authorID]  
 $cooldown[5m;**$customEmoji[emoji_999] <@$authorID>,bu komutu kullanmak için \`5 Dakika\` beklemelisin.**] 
 `
