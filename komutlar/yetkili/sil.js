8ffc0528aa6e9bb063f58ea56774509f331b028c
Glitch (westabot21xx) <none>
2022-06-02 10:59:58 +0000

    ⛪️👽 Checkpoint ./komutlar/yetkili/sil.js:38852640/15

--- komutlar/yetkili/sil.js
+++ komutlar/yetkili/sil.js
@@ -2,9 +2,9 @@ module.exports = {
 name: "sil",
 code: `
 $clear[$message[1]]
-**$message[1]** adet mesaj silindi.
+**$message[1]** adet mesaj başarıyla silindi.
 $argsCheck[1;**$customEmoji[emoji_999] <@$authorID>, bir sayı yazmalısın.**]
-$onlyPerms[managemessages;**$customEmoji[emoji_999] <@$authorID>, bu komutu kullanmak için \`Mesajları Yönet\` yetkisine sahip olmalısın.]]
+$onlyPerms[managemessages;**$customEmoji[emoji_999] <@$authorID>, bu komutu kullanmak için \`Mesajları Yönet\` yetkisine sahip olmalısın.]
 $deletecommand
 $onlyIf[$isNumber[$message[1]]!=false;]
 $suppressErrors  
