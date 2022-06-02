cc8fd152025a36c765e0f10866c30f46b3d62a58
Glitch (westabot21xx) <none>
2022-06-02 18:39:08 +0000

    💓🔖 Checkpoint ./komutlar/yardım/yardım.js:38852640/227

--- komutlar/yardım/yardım.js
+++ komutlar/yardım/yardım.js
@@ -3,17 +3,17 @@ name:"yardım",
 code:`
 $author[1;$userTag;$authorAvatar]
 $color[1;RANDOM]
-$addField[1;**<:boteglence:968593132453261322>・Not**;**Not : Öneri veya Destek almak için \`$getServerVar[prefix]destek\` & \`$getServerVar[prefix]öneri\` yazarak önerinizi veya şikayetinizi bildirebilirsiniz.**]
+$addField[1;**<:boteglence:981889424881238036>・Not**;**Not : Öneri veya Destek almak için \`$getServerVar[prefix]destek\` & \`$getServerVar[prefix]öneri\` yazarak önerinizi veya şikayetinizi bildirebilirsiniz.**]
 $description[1;**$customEmoji[tatlimisir] Merhaba ben \`$username[$clientID]\` komutlarımı görmek için menülere bakarak komutlarıma ulaşabilirsin.**]
 
-$addField[1;**<:gift:968571121458872370>・Ekonomi**;
+$addField[1;**<:gifts:981889629819142204>・Ekonomi**;
 \`?param / ?günlük / ?çalış / ?kazı-kazan / ?balık-tut / ?kasa-al / ?kasa-aç / ?para-yatır / ?para-çek / ?şanslı-sayı oyunu / ?para-sıralaması / ?para-gönder
 \`
-$addField[1;**<:admin:977852980990726154>・Yetkili**;
+$addField[1;**<:adminbadge:981888773921046578>・Yetkili**;
 \`?ban / ?ban-say / ?kick / ?nuke / ?sil / ?hgbb aç / ?hgbb kapat / ?otorol aç / ?otorol kapat
 \`
 ]
-$addField[1;**<:uye:977856238056534047>・Kullanıcı**;
+$addField[1;**<:zil2:981890362224287795>・Kullanıcı**;
 \`?avatar / ?sunucu-avatar / ?komut-sayısı / ?öneri / ?destek
 \`
 ]
