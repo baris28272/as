21012819eaa66ddb4cde6468556676f35622bb79
Glitch (westabot21xx) <none>
2022-06-02 05:06:17 +0000

    📚💨 Checkpoint ./komutlar/yetkili/otorol-aç.js:38852640/390

--- komutlar/yetkili/otorol-aç.js
+++ komutlar/yetkili/otorol-aç.js
@@ -4,11 +4,11 @@ code:`
 $author[1;$userTag;$authorAvatar]
 $color[1;RANDOM]
 $description[1;**$customEmoji[emoji99] <@$authorID> Otorol sistemi başarıyla açıldı**]
-$onlyIf[$mentionedRoles[1]!=;]
-$onlyIf[$mentionedChannels[1]!=;]
-$argsCheck[>2;**$customEmoji[emoji_999] <@$authorID>, ilk önce bir kanal etiketlemelisin ve bir rol etiketlemelisin.**]
 $setServerVar[otorol;$mentionedRoles[1]]
 $setServerVar[otokanal;$mentionedChannels[1]]
+$argsCheck[>1;**$customEmoji[emoji_999] <@$authorID>, ilk önce bir kanal etiketlemelisin ve bir rol etiketlemelisin.**]
+$onlyIf[$mentionedRoles[1]!=;]
+$onlyIf[$mentionedChannels[1]!=;]
 $onlyPerms[admin;**$customEmoji[emoji_999] <@$authorID> Bu komutu kullanmak için \`Yönetici İznin\` olmalı**]
 `
 }
