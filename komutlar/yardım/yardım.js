090eea8a5688409a183bb3158c6fbf35f4eb31c8
Glitch (westabot21xx) <none>
2022-06-05 18:41:21 +0000

    👆🛫 Checkpoint ./komutlar/yetkili/prefix.js:38852640/76 ./komutlar/yardım/yardım.js:38852640/688

--- komutlar/yardım/yardım.js
+++ komutlar/yardım/yardım.js
@@ -1,5 +1,6 @@
 module.exports = {
 name:"yardım",
+aliases:["yardim","help"],
 code:`
 $author[1;$userTag;$authorAvatar]
 $color[1;RANDOM]
@@ -7,14 +8,14 @@ $addField[1;**<:boteglence:981889424881238036>・Not**;**Not : Öneri veya Deste
 $description[1;**$customEmoji[tatlimisir] Merhaba ben \`$username[$clientID]\` komutlarımı görmek için menülere bakarak komutlarıma ulaşabilirsin.**]
 
 $addField[1;**<:gifts:981889629819142204>・Ekonomi**;
-\`?param / ?günlük / ?çalış / ?kazı-kazan / ?balık-tut / ?kasa-al / ?kasa-aç / ?para-yatır / ?para-çek / ?şanslı-sayı oyunu / ?para-sıralaması / ?para-gönder
+\`$getServerVar[prefix]param / $getServerVar[prefix]günlük / $getServerVar[prefix]çalış / $getServerVar[prefix]kazı-kazan / $getServerVar[prefix]balık-tut / $getServerVar[prefix]kasa-al / $getServerVar[prefix]kasa-aç / $getServerVar[prefix]para-yatır / $getServerVar[prefix]para-çek / $getServerVar[prefix]şanslı-sayı oyunu / $getServerVar[prefix]para-sıralaması / $getServerVar[prefix]para-gönder
 \`
 $addField[1;**<:adminbadge:981888773921046578>・Yetkili**;
-\`?ban / ?ban-say / ?kick / ?nuke / ?sil / ?hgbb aç / ?hgbb kapat / ?otorol aç / ?otorol kapat
+\`$getServerVar[prefix]ban / $getServerVar[prefix]ban-say / $getServerVar[prefix]kick / $getServerVar[prefix]nuke / $getServerVar[prefix]sil / $getServerVar[prefix]hgbb aç / $getServerVar[prefix]hgbb kapat / $getServerVar[prefix]otorol aç / $getServerVar[prefix]otorol kapat / $getServerVar[prefix]prefix ayarla
 \`
 ]
 $addField[1;**<:zil2:981890362224287795>・Kullanıcı**;
-\`?avatar / ?sunucu-avatar / ?komut-sayısı / ?öneri / ?destek / ?afk
+\`$getServerVar[prefix]avatar / $getServerVar[prefix]sunucu-avatar / $getServerVar[prefix]öneri / $getServerVar[prefix]destek / $getServerVar[prefix]afk
 \`
 ]
 
--- komutlar/yetkili/prefix.js
+++ komutlar/yetkili/prefix.js
@@ -1,10 +1,10 @@
 module.exports = {
 name:"prefix ayarla",
 code:`
-$author[1:$userTag;$authorAvatar]
+$author[1;$userTag;$authorAvatar]
 $color[1;RANDOM]
-$description[1;**$customEmoji[emoji99] <@$authorID> Sunucu prefixi \`$message[1]\` olarak ayarlandı.**]
-$setServerVar[prefix;$message[1]
-$onlyPerms[admin;**$customEmoji[emoji_999] <@$authorID> Bu komutu 
+$description[1;**$customEmoji[emoji99] <@$authorID> Sunucu prefixi \`$message\` olarak ayarlandı.**]
+$setServerVar[prefix;$message]
+$onlyPerms[admin;**$customEmoji[emoji_999] <@$authorID> Bu komutu sadece \`Yönetici İzni\` olan kişiler kullanabilir.**]
 `
   }
